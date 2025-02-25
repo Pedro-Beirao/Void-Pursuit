02bd7c711ff363784a74ea6cce84c26ef41b165f
Pedro Beirao <pedrocbeirao@gmail.com>
2025-02-25 19:46:07 +0000

    Check boundaries on movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,13 +4,29 @@ public class Movement : MonoBehaviour
 {
     public float speed = 5;
 
+    [Header("Boundary")]
+    public Vector2 top_left;
+    public Vector2 bottom_right;
+
     void Update()
     {
-        Vector3 dir = Vector3.zero;
+        Vector3 newPos = this.transform.position;
 
-        dir.y += Input.GetAxis("Vertical");
-        dir.x += Input.GetAxis("Horizontal");
+        Vector3 vertical = new Vector3(0, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+        if (CheckBounds(this.transform.position + vertical))
+            this.transform.position += vertical;
 
-        this.transform.position += dir * speed * Time.deltaTime;
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        if (CheckBounds(this.transform.position + horizontal))
+            this.transform.position += horizontal;
+    }
+
+    bool CheckBounds(Vector3 newPos)
+    {
+        if (newPos.x > top_left.x && newPos.x < bottom_right.x &&
+            newPos.y > bottom_right.y && newPos.y < top_left.y)
+            return true;
+
+        return false;
     }
 }
