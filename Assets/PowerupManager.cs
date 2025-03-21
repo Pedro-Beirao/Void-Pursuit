ce452c1d6d0bda0c3c4419fb8a0a641902706e64
Pedro Beirao <pedrocbeirao@gmail.com>
2025-03-21 02:30:32 +0000

    Add ammo pick ups

--- Assets/PowerupManager.cs
+++ Assets/PowerupManager.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PowerupManager : MonoBehaviour
 {
     Health health;
+    Shooting shooting;
 
     void Start()
     {
         health = GetComponent<Health>();
+        shooting = GetComponent<Shooting>();
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -17,6 +19,11 @@ public class PowerupManager : MonoBehaviour
             collider.transform.parent = transform;
             collider.transform.localPosition = Vector3.zero;
             Destroy(collider);
+        }
+        else if (collider.gameObject.CompareTag("Ammo"))
+        {
+            shooting.ammo += 20;
+            Destroy(collider.gameObject);
         }
     }
 }
