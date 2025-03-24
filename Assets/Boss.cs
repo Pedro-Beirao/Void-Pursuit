a8b320a7b5dc7291461b3bbb3966fc18a8326b52
Pedro Beirao <pedrocbeirao@gmail.com>
2025-03-24 19:12:12 +0000

    Boss destruction

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class Boss : MonoBehaviour
 {
@@ -12,16 +13,27 @@ public class Boss : MonoBehaviour
     [SerializeField] GameObject shield;
 
     [Header("Final")]
-    [SerializeField] GameObject gameUI;
     [SerializeField] GameObject finalUI;
+    [SerializeField] TMP_Text scoreText;
+
+    float elapsedTime = 0;
+    int score = 0;
+    bool youwon = false;
 
     void Update()
     {
+
+        elapsedTime += Time.deltaTime;
+        score = KillCounter.killCount * 20 - ((int)elapsedTime);
+
         if (!topBomb && !bottomBomb && shield)
             Destroy(shield);
 
-        if (!main)
+        if (!main && !youwon)
+        {
+            youwon = true;
             Final();
+        }
 
         if (transform.position.x < 7.5) return;
         transform.position += Vector3.left * speed * Time.deltaTime;
@@ -29,7 +41,8 @@ public class Boss : MonoBehaviour
 
     void Final()
     {
-        gameUI.SetActive(false);
+        // gameUI.SetActive(false);
+        scoreText.text = "Score: " + score.ToString();
         finalUI.SetActive(true);
     }
 }
