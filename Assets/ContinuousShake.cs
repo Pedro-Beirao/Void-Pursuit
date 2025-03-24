4fec5eb7d6646e7a4ddc45d35bb787434dc25d0b
Pedro Beirao <pedrocbeirao@gmail.com>
2025-03-24 21:55:13 +0000

    New menu look

---
+++ Assets/ContinuousShake.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Vector3 originalPos;
+    public float shakeAmmount = 1;
+    public float shakeDampening = 0.2f;
+
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, originalPos + Random.insideUnitSphere * shakeAmmount, shakeDampening);
+    }
+}
